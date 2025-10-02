c1ef7eaea49005a9e27f3ef3fb267d1518babe15
Om Pimple <ompimple04@gmail.com>
2025-10-02 17:11:48 +0530

    Modified main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ from utils import log_message
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
 
 def main():
-    log_message("🚀 Starting Zensar AI Data Pipeline Simulation")
+    log_message("🚀 Starting AI Data Pipeline Simulation")
 
     # Step 1: Collect Data
     collected_files = collect_data()
